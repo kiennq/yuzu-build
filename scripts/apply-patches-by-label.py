3a0970c0aa67a4c3a8bfb01348e0601cb5d6acfc
Kien Nguyen <kien.n.quang@gmail.com>
2023-05-09 13:29:07 +0900

    build EA and excluding do-not-merge

---
+++ scripts/apply-patches-by-label.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: 2019 yuzu Emulator Project
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# Download all pull requests as patches that match a specific label
+# Usage: python apply-patches-by-label.py <label-to-match> <label-to-exclude> <tagline>
+
+import json, requests, subprocess, sys, traceback
+
+tagline = sys.argv[3]
+
+def check_individual(labels):
+    found = False
+    for label in labels:
+        if (label["name"] == sys.argv[1]):
+            found = True
+        elif (label["name"] == sys.argv[2]):
+            return False
+    return found
+
+def do_page(page):
+    url = f"https://api.github.com/repos/yuzu-emu/yuzu/pulls?page={page}"
+    response = requests.get(url)
+    response.raise_for_status()
+    if (response.ok):
+        j = json.loads(response.content)
+        if j == []:
+            return
+        for pr in j:
+            if (check_individual(pr["labels"])):
+                pn = pr["number"]
+                print(f"Matched PR# {pn}")
+                print(subprocess.check_output(["git", "fetch", "https://github.com/yuzu-emu/yuzu.git", f"pull/{pn}/head:pr-{pn}", "-f", "--no-recurse-submodules"]))
+                print(subprocess.check_output(["git", "merge", "--squash", f"pr-{pn}"]))
+                print(subprocess.check_output(["git", "commit", f"-m\"Merge {tagline} PR {pn}\""]))
+
+try:
+    for i in range(1,10):
+        do_page(i)
+except:
+    traceback.print_exc(file=sys.stdout)
+    sys.exit(-1)
