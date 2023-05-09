5e11e3b3f881ca4820eb5a35bac7b0c5122c4266
Kien Nguyen <kien.n.quang@gmail.com>
2023-05-09 15:59:12 +0900

    cleanup, only upload 7z and sources file

--- scripts/windows/upload.ps1
+++ scripts/windows/upload.ps1
@@ -13,16 +13,12 @@ else {
     $RELEASE_DIST = "yuzu-windows-msvc-$BUILD_NAME"
 }
 
-$MSVC_BUILD_ZIP = "yuzu-windows-msvc-$GITDATE-$GITREV.zip" -replace " ", ""
 $MSVC_BUILD_PDB = "yuzu-windows-msvc-$GITDATE-$GITREV-debugsymbols.zip" -replace " ", ""
 $MSVC_SEVENZIP = "yuzu-windows-msvc-$GITDATE-$GITREV.7z" -replace " ", ""
-$MSVC_TAR = "yuzu-windows-msvc-$GITDATE-$GITREV.tar" -replace " ", ""
-$MSVC_TARXZ = "yuzu-windows-msvc-$GITDATE-$GITREV.tar.xz" -replace " ", ""
 $MSVC_SOURCE = "yuzu-windows-msvc-source-$GITDATE-$GITREV" -replace " ", ""
 $MSVC_SOURCE_TAR = "$MSVC_SOURCE.tar"
 $MSVC_SOURCE_TARXZ = "$MSVC_SOURCE_TAR.xz"
 
-$env:BUILD_ZIP = $MSVC_BUILD_ZIP
 $env:BUILD_SYMBOLS = $MSVC_BUILD_PDB
 $env:BUILD_UPDATE = $MSVC_SEVENZIP
 
@@ -55,11 +51,7 @@ Copy-Item "$BUILD_DIR\*" -Destination $RELEASE_DIST -Recurse
 rm "$RELEASE_DIST\*.exe"
 Get-ChildItem "$BUILD_DIR" -Recurse -Filter "yuzu*.exe" | Copy-Item -destination $RELEASE_DIST
 Get-ChildItem "$BUILD_DIR" -Recurse -Filter "QtWebEngineProcess*.exe" | Copy-Item -destination $RELEASE_DIST
-7z a -tzip $MSVC_BUILD_ZIP $RELEASE_DIST\*
 7z a $MSVC_SEVENZIP $RELEASE_DIST
-
-7z a -r -ttar $MSVC_TAR $RELEASE_DIST
-7z a -r -txz $MSVC_TARXZ $MSVC_TAR
 
 Get-ChildItem . -Filter "*.zip" | Copy-Item -destination "artifacts"
 Get-ChildItem . -Filter "*.7z" | Copy-Item -destination "artifacts"
