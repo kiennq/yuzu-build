3cb65d1fef0bd8d676bf9338592a0dc34c54578e
Kien Nguyen <kien.n.quang@gmail.com>
2023-05-11 10:06:37 +0900

    ci: include release notes of which PRs are included

--- scripts/merge/apply-patches-by-label.py
+++ scripts/merge/apply-patches-by-label.py
@@ -8,6 +8,7 @@ import json, requests, subprocess, sys, traceback, os
 
 tagline = sys.argv[3]
 token = os.getenv('GITHUB_TOKEN')
+merged_prs = []
 
 def check_individual(labels):
     found = False
@@ -20,7 +21,7 @@ def check_individual(labels):
 
 def do_page(page):
     url = f"https://api.github.com/repos/yuzu-emu/yuzu/pulls?page={page}"
-    headers = {'authorization': 'Bearer %s' % token} if token is not None else None
+    headers = {'authorization': f"Bearer {token}"} if token is not None else None
     response = requests.get(url, headers=headers)
     response.raise_for_status()
     if (response.ok):
@@ -34,10 +35,21 @@ def do_page(page):
                 print(subprocess.check_output(["git", "fetch", "https://github.com/yuzu-emu/yuzu.git", f"pull/{pn}/head:pr-{pn}", "-f", "--no-recurse-submodules"]))
                 print(subprocess.check_output(["git", "merge", "--squash", f"pr-{pn}"]))
                 print(subprocess.check_output(["git", "commit", f"-m\"Merge {tagline} PR {pn}\""]))
+                merged_prs.append(pr["html_url"])
+
+def release_note(file, prs):
+    if prs == []:
+        return
+    file = open(file, 'w')
+    file.write('This release include these EA PRs: \n')
+    for pr in prs:
+        file.write(f"  - {pr}\n")
+    file.close()
 
 try:
     for i in range(1,10):
         do_page(i)
+    release_note(f"{os.getenv('GITHUB_WORKSPACE')}-CHANGELOG.txt", merged_prs)
 except:
     traceback.print_exc(file=sys.stdout)
     sys.exit(-1)
